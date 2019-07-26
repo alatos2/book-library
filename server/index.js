73d86a6117b828522aff70e2959e41f9bfcae14e
alatos2 <alabitosin60@gmail.com>
2019-07-27 00:59:32 +0100

    Worked on template engines

--- server/index.js
+++ server/index.js
@@ -13,10 +13,27 @@ app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/d
 app.use('/js', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/js/')));
 app.use('/js', express.static(path.join(__dirname, '../node_modules/jquery/dist/')));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  // res.send('Welcome to node world');
-  res.sendFile(path.join(__dirname, '../views', 'index.html'));
+  res.render('index', {
+    title: 'Grace is all I need',
+    bio: [
+      {
+        first_name: 'Alabi',
+        last_name: 'Tosin',
+      },
+      {
+        first_name: 'Kayode',
+        last_name: 'Martins',
+      },
+      {
+        first_name: 'Kenny',
+        last_name: 'Red',
+      },
+    ],
+  });
 });
 
 dotenv.config();
